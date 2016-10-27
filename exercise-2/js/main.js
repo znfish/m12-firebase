e8ba158012874cb377f6b1fe26a27d125fb420a4
Christy <hyl9@uw.edu>
2016-10-27 10:15:03 -0700

    1-1

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -2,23 +2,34 @@
 $(function() {
 
 	// Setup: Initialize Firebase using the configuration of your project
-
+	var config = {
+    	apiKey: "AIzaSyADfDcs4uu_hmbda44OvLw9x3JC_aEpr5g",
+    	authDomain: "mm12-2.firebaseapp.com",
+    	databaseURL: "https://mm12-2.firebaseio.com",
+    	storageBucket: "mm12-2.appspot.com",
+    	messagingSenderId: "699360607882"
+  		};
+  	firebase.initializeApp(config);
 
 	// Set a reference to a "photos" point in your database
+	var pic = firebase.database.ref('pic');
 
 
 	// Create a variable to store the firebase storage object
-
+	var storage = firebase.storage();
+	var fileRef = storage.ref('filename');
 
 	// Set listener: when an child is added, render each photo
-
+	fileRef.on('childAdded').then(function(snapshot) {
 		// Get the value of the data
-
-
+		var data = snapshot.val();
 		// Using jQuery, create a new img element with the URL of your data
-
-
+		var img = $('<img>')
 		// Append your img to your element with id photos
+		$('#photo').append();
+	});
+
+
 
 
 
@@ -26,6 +37,7 @@ $(function() {
 	// Reading Data: Form submission
 
 		// Get the file
+		var file = $()
 
 
 		// Create a reference on Firebase storage using the filename
